cc26e568f3839ea434fdaf85a2db42057bb20e8c
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-31 11:40:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,10 +8,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-function greet(h,m){
-  document.getElementById('time');
-  if (x<12 && m==0)
-    alert 'Good Morning'
+function greet(time){
+  if (time <12){
+  return ("good morning")
+}
+  else if (time >12 && time <17) {
+  return ("good Evening")
+}
+  
+  else (){
+    
+  }
 
 }
 console.log(greet(1,0));
