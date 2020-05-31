5ea75f43c69f9ff7b8eaf694da91ba33880fbf91
Sara Nooraldin <sarasaadiq98@gmail.com>
2020-05-31 11:34:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,4 +8,11 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(h,m){
+  document.getElementById('time');
+  if (x<12 && m==0)
+    alert 'Good Morning'
+
+}
+console.log(greet(1,0));
 /* Write your implementation of displayMessage() */
